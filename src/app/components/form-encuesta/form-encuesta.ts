a4b6227e93de5607f1844c1c9a6ebb20d78109dd
Luke <suzuguenza@gmail.com>
2025-11-08 23:49:43 -0300

    actualización de vistas

--- src/app/components/form-encuesta/form-encuesta.ts
+++ src/app/components/form-encuesta/form-encuesta.ts
@@ -1,21 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { FormControl, FormGroup, FormsModule } from "@angular/forms";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 @Component({
   selector: 'app-form-encuesta',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './form-encuesta.html',
   styleUrl: './form-encuesta.scss'
 })
 export class FormEncuesta {
-   form = new FormGroup({
+  protected seleccion = signal<string | null>(null);
+  protected form = new FormGroup({
     seleccion: new FormControl('')
-  });
-
-  protected opciones = ['Excelente', 'Buena', 'Regular', 'Mala'];
-  protected enviada = false;
+  })
+ protected opciones = ['Excelente', 'Buena', 'Regular', 'Mala'];
+  protected enviada = signal(false);
 
   enviar() {
-    if (this.form.value.seleccion) this.enviada = true;
+    if (this.seleccion()) this.enviada.set(true);
+    this.form.patchValue({seleccion: this.seleccion()})
   }
   
 }
---
+++ src/app/components/pre-registro/pre-registro.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-registro',
+  imports: [],
+  templateUrl: './pre-registro.html',
+  styleUrl: './pre-registro.scss'
+})
+export class PreRegistro {
+
+}
